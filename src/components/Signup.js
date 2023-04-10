57bd4de2e9edf804a85c69ee7c812716ab57baa5
Chetan Jain <chetanplrch@gmail.com>
2023-04-09 23:08:23 -0400

    update the signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,17 @@
 import * as React from "react";
-import { signUpUser } from "../api/index";
+import { createUserAccount } from "../api/index";
 import "./Signup.css";
 import CustomTextField from "./CustomTextField";
-import h from '../helper/index';
+import h from "../helper/index";
 
-import IconButton from '@mui/material/IconButton';
-import InputAdornment from '@mui/material/InputAdornment';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import CustomSelect from "./CustomSelect";
-import CustomCheckbox from './CustomCheckbox';
-import Button from '@mui/material/Button';
-import CircularProgress from '@mui/material/CircularProgress';
+import CustomCheckbox from "./CustomCheckbox";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const defaultUser = {
   username: "",
@@ -30,7 +30,7 @@ const SignUp = (props) => {
   const [user, setUser] = React.useState(defaultUser);
   const [errors, setErrors] = React.useState(defaultUser);
   const [showPassword, setShowPassword] = React.useState(false);
-  const [saving, setSaving] = React.useState(null)
+  const [saving, setSaving] = React.useState(null);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -38,17 +38,18 @@ const SignUp = (props) => {
 
   const onChangeOfValue = (key, value) => setUser({ ...user, [key]: value });
 
-  const createUser = () => {
-      setSaving(true)
-  }
+  const createUser = async () => {
+    setSaving(true);
+    const response = await createUserAccount(user);
+    console.log("----", response);
+    setSaving(false);
+  };
 
-  const onBlur = () => setErrors(h.validator(user))
-    console.log('user', user)
+  const onBlur = () => setErrors(h.validator(user));
+
   return (
     <div className="dialog">
-        <div className="dialog-header">
-          Sign up here!
-        </div>
+      <div className="dialog-header">Sign up here!</div>
       <CustomTextField
         onBlur={onBlur}
         error={errors.username.helperText}
@@ -90,7 +91,8 @@ const SignUp = (props) => {
         value={user.password}
         onChange={onChangeOfValue}
         inputProps={{
-            endAdornment: <InputAdornment position="end">
+          endAdornment: (
+            <InputAdornment position="end">
               <IconButton
                 aria-label="toggle password visibility"
                 onClick={handleClickShowPassword}
@@ -100,48 +102,56 @@ const SignUp = (props) => {
                 {showPassword ? <VisibilityOff /> : <Visibility />}
               </IconButton>
             </InputAdornment>
+          ),
         }}
       />
-      <CustomSelect 
+      <CustomSelect
         onBlur={onBlur}
         error={errors.gender.helperText}
         helperText={errors.gender.helperText}
         name="gender"
         value={user.gender}
         onChange={onChangeOfValue}
-        options={[{
-            label: 'MALE',
-            value: 'MALE'
-        }, {
-            label: 'FEMALE',
-            value: 'FEMALE'
-        }, {
-            label: 'OTHER',
-            value: 'OTHER'
-        }]}
+        options={[
+          {
+            label: "MALE",
+            value: "MALE",
+          },
+          {
+            label: "FEMALE",
+            value: "FEMALE",
+          },
+          {
+            label: "OTHER",
+            value: "OTHER",
+          },
+        ]}
       />
-      <CustomTextField 
+      <CustomTextField
         onBlur={onBlur}
         error={errors.age.helperText}
         helperText={errors.age.helperText}
-        name="age" 
-        value={user.age} 
+        name="age"
+        value={user.age}
         onChange={onChangeOfValue}
-    />
-      <CustomSelect 
+      />
+      <CustomSelect
         onBlur={onBlur}
         error={errors.role.helperText}
         helperText={errors.role.helperText}
         name="role"
         value={user.role}
         onChange={onChangeOfValue}
-        options={[{
-            label: 'LISTENER',
-            value: 'LISTENER'
-        }, {
-            label: 'SEEKER',
-            value: 'SEEKER'
-        }]}
+        options={[
+          {
+            label: "LISTENER",
+            value: "LISTENER",
+          },
+          {
+            label: "SEEKER",
+            value: "SEEKER",
+          },
+        ]}
       />
       <CustomTextField
         onBlur={onBlur}
@@ -159,18 +169,23 @@ const SignUp = (props) => {
         value={user.state}
         onChange={onChangeOfValue}
       />
-      <CustomCheckbox 
+      <CustomCheckbox
         className="anonymous-input"
         name="isAnonymous"
         checked={user.isAnonymous}
         onChange={onChangeOfValue}
-        text={'Stay anonymous'}
+        text={"Stay anonymous"}
       />
       <Button
-        onClick={createUser} 
-        variant={saving ? "outlined" : "contained" }
-        color="success">
-        {saving ? <CircularProgress size={25} color="success" /> : 'Create account'}
+        onClick={createUser}
+        variant={saving ? "outlined" : "contained"}
+        color="success"
+      >
+        {saving ? (
+          <CircularProgress size={25} color="success" />
+        ) : (
+          "Create account"
+        )}
       </Button>
     </div>
   );
