32260497c96c3f56b5b3c5662a4db87f0c066d0a
Pooja Mule <poojamule@poojas-mbp.myfiosgateway.com>
2023-04-11 12:58:46 -0400

    [Profile UI] User Profile page - component and UI

---
+++ src/components/profile/index.js
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+
+import './profile.css'
+// import CustomTextField from "../CustomTextField.js";
+import CustomCheckbox from "../CustomCheckbox";
+
+const Profile = () => {
+  const [profile, setProfile] = useState({
+    firstName: 'John',
+    lastName: 'Wick',
+    username: 'myBeagleDaisy',
+    isAnonymous: true,
+    email: 'wickjohn@continental.org',
+    gender: 'Male',
+    age: 50,
+    city: 'New York',
+    state: 'New York',
+    role: 'Seeker'
+  });
+
+  return (
+    <div className='container'>
+      <h1>User: {profile.username}</h1>
+      <div className='input-container'>
+        <label className='label'>Username</label>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Username"
+          value={profile.username}
+          onChange={(e) => console.log('do something')}
+        />
+      </div>
+
+      <div className='input-container'>
+        <label className='label'>First Name</label>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="First Name"
+          value={profile.firstName}
+          onChange={(e) => console.log('do something')}
+        />     
+      </div>
+
+      <div className='input-container'>
+        <label className='label'>Last Name</label>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Last Name"
+          value={profile.lastName}
+          onChange={(e) => console.log('do something')}
+        />
+      </div>
+
+      <div className='input-container'>
+        <label className='label'>Email</label>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Last Name"
+          value={profile.email}
+          onChange={(e) => console.log('do something')}
+        />
+      </div>
+
+      <div className='input-container'>
+        <label className='label'>Gender</label>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Last Name"
+          value={profile.gender}
+          onChange={(e) => console.log('do something')}
+        />
+      </div>
+
+
+      <div className='input-container'>
+        <label className='label'>Age</label>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Last Name"
+          value={profile.age}
+          onChange={(e) => console.log('do something')}
+        />
+      </div>
+
+      <div className='input-container'>
+        <label className='label'>City</label>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Last Name"
+          value={profile.city}
+          onChange={(e) => console.log('do something')}
+        />
+      </div>
+
+      <div className='input-container'>
+        <label className='label'>State</label>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Last Name"
+          value={profile.state}
+          onChange={(e) => console.log('do something')}
+        />
+      </div>
+
+      <div className='input-container'>
+        <label className='label'>Role</label>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Last Name"
+          value={profile.role}
+          onChange={(e) => console.log('do something')}
+          readOnly
+        />
+      </div>
+      
+      <CustomCheckbox
+        className="anonymous-input"
+        name="isAnonymous"
+        checked={profile.isAnonymous}
+        onChange={() => {console.log('do something')}}
+        text={"Anonymous"}
+      />
+    </div>
+  );
+};
+
+export default Profile;
