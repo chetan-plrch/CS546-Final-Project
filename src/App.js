0eeefe603bb253a84f4c23da34e46bbf5c33bcbc
Pooja Mule <poojavm1595@gmail.com>
2023-04-20 13:02:56 -0400

    [Connection component] Skeleton

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import HomePage from './layouts/HomePage'
 import Login from './components/login';
-import Profile from './components/profile/index.js'
+import Profile from './components/profile'
+import Connections from './components/connections';
 import './App.css';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
           <Route exact path="/" element={ <HomePage /> } />
           <Route exact path="/user/login" element={ <Login /> } />
           <Route exact path="/user/profile" element={ <Profile /> } />
+          <Route exact path="/user/connections" element={ <Connections /> } />
         </Routes>
       </BrowserRouter>
     </div>
