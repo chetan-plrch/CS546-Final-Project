9a453ef4e08b8573357145c343b7aad0e7480029
Chetan Jain <chetanplrch@gmail.com>
2023-04-21 19:21:06 -0400

    feat: add chat component

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -1,4 +1,4 @@
-import { sockets, chats } from '../config/mongoCollections.js'
+import { sockets } from '../config/mongoCollections.js'
 import { Server } from 'socket.io'
 import { addMessagesToChat, mapSocketIdToUser } from '../data/chat.js'
 
@@ -16,11 +16,28 @@ const ioS = new Server(3002, {
 //     next()
 // });
 
+ioS.on("connection", async socket => {
+    const userId = socket.handshake.query.userId
+    if (userId) {
+        console.log('Socket connection is live!!')
+        const created = await mapSocketIdToUser({ userId, socketId: socket.id })
+
+        if (created) {
+            socket.on("message", async ({ senderId, receiverId, message }) => {
+                console.log('on message', senderId, receiverId, message)
+                await sendMessage(senderId, receiverId, message)
+            })
+        } else {
+            console.log('ERROR: Failed to create socket mapping with the user', userId)
+        }
+    }
+})
+
 const sendMessage = async (senderId, receiverId, message) => {
     const socketsCtx = await sockets()
     const socketMapping = await socketsCtx.findOne({ userId: receiverId })
     if (socketMapping) {
-        ioS.to(socketMapping.socketId).emit('message', message)
+        ioS.to(socketMapping.socketId).emit('message', { senderId, receiverId, message } )
     } else {
         const socketMap = await socketsCtx.findOne({ userId: senderId })
         if (socketMap) {
@@ -36,22 +53,5 @@ const sendMessage = async (senderId, receiverId, message) => {
         console.log('ERROR: Failed to save message to database', senderId)
     }
 }
-
-ioS.on("connection", async socket => {
-    console.log('Socket connection is live!!')
-
-    const userId = '6438d7ac8e1c21e45686e198'
-    const created = await mapSocketIdToUser({ userId, socketId: socket.id })
-
-    if (created) {
-        socket.on("message", async ({ senderId, receiverId, message }) => {
-            console.log('on message', senderId, receiverId, message)
-            await sendMessage(senderId, receiverId, message)
-        })
-    } else {
-        console.log('ERROR: Failed to create socket mapping with the user', userId)
-    }
-
-})
 
 export default ioS
