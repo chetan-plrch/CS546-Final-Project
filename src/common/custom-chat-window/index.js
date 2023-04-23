a768fc3401449e0de1c8a4210bb6328d57c7fd67
Pooja Mule <poojavm1595@gmail.com>
2023-04-23 12:32:38 -0400

    [Connections page] Chat component update

--- src/common/custom-chat-window/index.js
+++ src/common/custom-chat-window/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css'
 import CustomButton from '../custom-button';
 import SearchIcon from '@mui/icons-material/Search';
+import CustomTextField from '../custom-textfield';
 
 export default function ChatWindow(props) {
 
-    const {allowSearch = false, allowBlocking = false, allowMessaging = false} = props;
+    const {allowSearch = false, allowBlocking = false, allowMessaging = false, chatHistory = {}} = props;
 
+    const [messages, setMessages] = useState(chatHistory?.conversation);
+    const [currentMessage, setCurrentMessage] = useState('');
+    // TODO - store logged in users id
+    const currentUserId = '644406498781b6017e69fb98';
     return (
         <div className='container'>
             <div className='header-container'>
@@ -23,12 +28,31 @@ export default function ChatWindow(props) {
                 }
             </div>
             <div className='chat-container'>
-                Chats go here
+                {messages?.map(function(item) {
+                    return <>
+                        {
+                            item?.senderId === currentUserId ? (
+                                // Current user sender - Show on right side
+                                <span className='sent-message' key={item.sentAt}>{item.message}</span>
+                            ) : (
+                                // other persons msg - show on left side
+                                <span className='received-message' key={item.sentAt}>{item.message}</span>
+                            )
+                        }
+                    </>
+                })}
             </div>
             {
                 allowMessaging ? (
                     <div className='footer-container'>
-                        <CustomButton title={'Type a message'} />
+                        <CustomTextField
+                            onBlur={() => {}}
+                            error={false}
+                            helperText={''}
+                            name='Type Message'
+                            value={currentMessage}
+                            onChange={(message) => setCurrentMessage(message)}
+                        />
                         <CustomButton title={'Send'} />
                     </div>
                 ) : null
