7c52a645a69736d3610cf70075b5cbc0cd7b0546
Chetan Jain <chetanplrch@gmail.com>
2023-04-26 14:15:10 -0400

    feat: add not found page and protect frontend routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import HomePage from './layouts/HomePage'
-import Login from './components/login';
 import Profile from './components/profile'
 import Connections from './components/connections';
+import Login from './components/login';
 import './App.css';
 import SignUp from './components/Signup';
 import Chat from '../src/common/custom-chat/index.js';
 import Chat2 from '../src/common/custom-chat/index2.js';
 import Feedback from './components/feedBack/feedback';
+import helper from './helper';
+import NotFound from './components/notfound';
 
 const App = () => {
+
+  useEffect(() => {
+    helper.checkLoggedIn()
+  }, [])
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={ <HomePage /> } />
-          <Route exact path="/user/signup" element={ <SignUp /> } />
-          <Route exact path="/user/login" element={ <Login /> } />
-          <Route exact path="/user/profile" element={ <Profile /> } />
+          <Route exact path="/signup" element={ <SignUp /> } />
+          <Route exact path="/login" element={ <Login /> } />
+          <Route exact path="/profile" element={ <Profile /> } />
           <Route exact path="/feedbacks" element = {<Feedback />} />
-          <Route exact path="/user/connections" element={ <Connections /> } />
+          <Route exact path="/connections" element={ <Connections /> } />
           <Route exact path="/chat-1" element={<Chat 
             user1={'6438d7ac8e1c21e45686e198'}
             user2={'6439851768f667131bd19b4a'}
@@ -29,6 +36,7 @@ const App = () => {
             user1={'6439851768f667131bd19b4a'}
             user2={'6438d7ac8e1c21e45686e198'}
           /> } />
+          <Route path='*' element={<NotFound />}/>
         </Routes>
       </BrowserRouter>
     </div>
