8772b7ef18b0f664c79fc2e3dc7848bcc0756663
Chetan Jain <chetanplrch@gmail.com>
2023-05-09 08:02:03 -0400

    feat: add spacing to all pages

--- src/layouts/HomePage.js
+++ src/layouts/HomePage.js
@@ -67,7 +67,7 @@ const Homepage = () => {
 
   return (
     <>
-      <Box pt={10} pb={5}>
+      <Box>
         <Tabs value={tabValue} onChange={handleChange}>
           <Tab label="Feeds" />
           <Tab label="Reviews" />
